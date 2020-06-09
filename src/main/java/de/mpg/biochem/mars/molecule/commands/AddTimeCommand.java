3daa658b2cbcb53dbdc3d883a406a564e449d31e
Karl Duderstadt <duderstadt@biochem.mpg.de>
2020-06-09 21:19:49 +0200

    When adding Time using an increment, set the time increment of all images to the one given.

--- src/main/java/de/mpg/biochem/mars/molecule/commands/AddTimeCommand.java
+++ src/main/java/de/mpg/biochem/mars/molecule/commands/AddTimeCommand.java
@@ -123,6 +123,11 @@ public class AddTimeCommand extends DynamicCommand implements Command {
 			archive.put(molecule);
 		});
 		
+		//Set the incrementTime for all metadata to match that provided.
+		archive.metadata().forEach(metadata -> 
+			metadata.images().forEach(image -> 
+				image.setTimeIncrementInSeconds(timeIncrement)));
+		
 		logService.info("Time: " + DoubleRounder.round((System.currentTimeMillis() - starttime)/60000, 2) + " minutes.");
 	    logService.info(LogBuilder.endBlock(true));
 	    
