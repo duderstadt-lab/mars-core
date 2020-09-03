103fc40b0832604fae5d6599cdfb731a68b6b816
Karl Duderstadt <duderstadt@biochem.mpg.de>
2020-09-03 21:04:37 +0200

    In progress commit for MoleculeArchiveTests.
    Now a test archive is created but unit tests have to be added still.

--- src/test/java/de/mpg/biochem/mars/molecule/MoleculeArchiveTests.java
+++ src/test/java/de/mpg/biochem/mars/molecule/MoleculeArchiveTests.java
@@ -2,16 +2,26 @@ package de.mpg.biochem.mars.molecule;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.scijava.table.DoubleColumn;
 
+import de.mpg.biochem.mars.metadata.MarsOMEChannel;
+import de.mpg.biochem.mars.metadata.MarsOMEImage;
 import de.mpg.biochem.mars.metadata.MarsOMEMetadata;
+import de.mpg.biochem.mars.metadata.MarsOMEPlane;
 import de.mpg.biochem.mars.table.MarsTable;
 import de.mpg.biochem.mars.table.MarsTableTests;
 import de.mpg.biochem.mars.util.MarsMath;
+import ome.xml.meta.OMEXMLMetadataRoot;
+import ome.xml.model.MapPair;
+import ome.xml.model.primitives.NonNegativeInteger;
+import ome.xml.model.primitives.PositiveInteger;
 
 public class MoleculeArchiveTests {
 
@@ -31,9 +41,9 @@ public class MoleculeArchiveTests {
 		SingleMoleculeArchive archive = new SingleMoleculeArchive("testMoleculeArchive");
 		
 		Random ran = new Random();
-		for (int i=0 ; i < 30; i++) {
+		for (int i=0 ; i < 100; i++) {
 			SingleMolecule molecule = new SingleMolecule(MarsMath.getUUID58());
-			molecule.setTable(generateRandomTable());
+			molecule.setTable(generateRandomTable(30));
 			if (ran.nextDouble() < 0.3)
 				molecule.addTag("below30");
 			if (ran.nextDouble() < 0.1)
@@ -41,13 +51,61 @@ public class MoleculeArchiveTests {
 			archive.put(molecule);
 		}
 		
-		archive.putMetadata(generateMetadata());
+		archive.putMetadata(generateMetadata(1, 1, 30));
 		
 		return archive;
 	}
 	
-	public static MarsOMEMetadata generateMetadata() {
+	public static MarsOMEMetadata generateMetadata(int cNum, int zNum, int tNum) {
 		MarsOMEMetadata metadata = new MarsOMEMetadata(MarsMath.getUUID58().substring(0, 10));
+		
+		MarsOMEImage image = new MarsOMEImage();
+		image.setImageIndex(0);
+		//image.setID(id);
+		//image.setPixelID(pixelID);
+		//image.setAquisitionDate(imageAquisitionDate);
+		//image.setName(imageName);
+		//image.setDescription(imageDescription);
+		
+		//create channels
+		for (int channelIndex=0; channelIndex < cNum; channelIndex++) {
+			MarsOMEChannel channel = new MarsOMEChannel();
+			image.setChannel(channel, channelIndex);
+		}
+		
+		//image.setPixelsPhysicalSizeX(pixelsPhysicalSizeX);
+		//image.setPixelsPhysicalSizeX(pixelsPhysicalSizeY);
+		//image.setPixelsPhysicalSizeX(pixelsPhysicalSizeZ);
+		
+		//image.setDimensionOrder(dimensionOrder);
+		
+		image.setSizeC(new PositiveInteger(cNum));
+		image.setSizeT(new PositiveInteger(tNum));
+		image.setSizeX(new PositiveInteger(300));
+		image.setSizeY(new PositiveInteger(300));
+		image.setSizeZ(new PositiveInteger(zNum));
+		
+		//Build Planes
+		//For now we just have one channel and one z point so plane number is tNum
+		/*
+		for (int t = 0; t < tNum; t++) {
+			MarsOMEPlane plane = new MarsOMEPlane();
+			plane.setC(new NonNegativeInteger(0));
+			plane.setT(new NonNegativeInteger(t));
+			plane.setZ(new NonNegativeInteger(0));
+			image.setPlane(plane, 0, 0, t);
+		}
+		
+		
+		if (md.getInstrumentCount() > 0) {
+			detectorSerialNumber = md.getDetectorSerialNumber(0, imageIndex);
+			detectorModel = md.getDetectorModel(0, imageIndex);
+			detectorManufacturer = md.getDetectorManufacturer(0, imageIndex);
+			detectorType = md.getDetectorType(0, imageIndex);
+		}
+		*/
+		
+		metadata.setImage(image, 0);
 		
 		return metadata;
 	}
@@ -70,7 +128,7 @@ public class MoleculeArchiveTests {
 		return table;
 	}
 	
-	public static MarsTable generateRandomTable() {
+	public static MarsTable generateRandomTable(int tNum) {
 		MarsTable table = new MarsTable();
 		DoubleColumn tCol = new DoubleColumn("T");
 		DoubleColumn xCol = new DoubleColumn("x");
@@ -79,7 +137,7 @@ public class MoleculeArchiveTests {
 		
 		Random ran = new Random();
 		
-		for (int t=0; t < MarsTableTests.XYNaNs.length; t++) {
+		for (int t=0; t < tNum; t++) {
 			tCol.add((double)t);
 			xCol.add(t + ran.nextGaussian() - 0.5);
 			yCol.add(t + ran.nextGaussian() - 0.5);
