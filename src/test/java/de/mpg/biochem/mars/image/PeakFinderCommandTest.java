e8e4e3054e562ee2c7c49ab597e11039f8eafc32
Karl Duderstadt <duderstadt@biochem.mpg.de>
2020-11-18 06:40:32 +0100

    add unit test for PeakFinderCommand

--- src/test/java/de/mpg/biochem/mars/image/PeakFinderCommandTest.java
+++ src/test/java/de/mpg/biochem/mars/image/PeakFinderCommandTest.java
@@ -1,33 +1,48 @@
 
 package de.mpg.biochem.mars.image;
 
-import java.io.IOException;
-import java.util.Random;
-
 import net.imagej.Dataset;
 import net.imagej.DatasetService;
 import net.imagej.axis.Axes;
 import net.imagej.axis.AxisType;
+import net.imagej.ops.OpService;
+import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.scijava.Context;
+import org.scijava.app.StatusService;
+import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 
+import de.mpg.biochem.mars.image.commands.PeakFinderCommand;
+import de.mpg.biochem.mars.table.MarsTable;
+import de.mpg.biochem.mars.table.MarsTableService;
 import de.mpg.biochem.mars.util.Gaussian2D;
+import de.mpg.biochem.mars.util.MarsMath;
 
 public class PeakFinderCommandTest {
+	
+	private static final double TOLERANCE = 0.01;
 
 	@Parameter
 	protected Context context;
 
 	@Parameter
 	protected DatasetService datasetService;
+	
+	@Parameter
+	protected LogService logService;
 
 	protected Context createContext() {
-		return new Context(DatasetService.class);
+		return new Context(DatasetService.class, StatusService.class, OpService.class, MarsTableService.class);
 	}
 
 	@BeforeEach
@@ -41,43 +56,123 @@ public class PeakFinderCommandTest {
 			context.dispose();
 			context = null;
 			datasetService = null;
+			logService = null;
 		}
 	}
 
 	@Test
-	@Order(1)
-	void findPeaks() throws IOException {
+	void findPeaksCommand() {
+		final PeakFinderCommand peakFinder = new PeakFinderCommand();
+
+		peakFinder.setContext(context);
+
+		peakFinder.setDataset(simulateDataset());
+		peakFinder.setUseROI(false);
+		peakFinder.setX0(0);
+		peakFinder.setY0(0);
+		peakFinder.setWidth(50);
+		peakFinder.setHeight(50);
+		peakFinder.setChannel(0);
+		peakFinder.setT(0);
+		peakFinder.setUseDogFiler(true);
+		peakFinder.setDogFilterRadius(1.8d);
+		peakFinder.setThreshold(50);
+		peakFinder.setMinimumDistance(4);
+		peakFinder.setFindNegativePeaks(false);
+		peakFinder.setGeneratePeakCountTable(false);
+		peakFinder.setGeneratePeakTable(true);
+		peakFinder.setMinimumRsquared(0);
+		peakFinder.setAddToRoiManager(false);
+		peakFinder.setProcessAllFrames(true);
+		peakFinder.setFitPeaks(true);
+		peakFinder.setFitRadius(4);
+		peakFinder.setIntegrate(true);
+		peakFinder.setIntegrationInnerRadius(1);
+		peakFinder.setIntegrationOuterRadius(3);
+
+		//Run the Command
+		peakFinder.run();
+
+		//Retrieve output from the command
+		//peakCountTable = peakFinder.getPeakCountTable()
+
+		MarsTable peakTable = peakFinder.getPeakTable();
+		
+		assertEquals(peakTable.getValue("T", 0), 0);
+		assertTrue( Math.abs( 10 - peakTable.getValue("x", 0) ) < TOLERANCE , "Peak x position is off by more than the tolerance. Should be 10 was " + peakTable.getValue("x", 0));
+		assertTrue( Math.abs( 10 - peakTable.getValue("y", 0) ) < TOLERANCE , "Peak y position is off by more than the tolerance. Should be 10 was " + peakTable.getValue("y", 0));
+		assertTrue( Math.abs( 15789.0 - peakTable.getValue("Intensity", 0) ) < TOLERANCE , "Peak Intensity is off by more than the tolerance. Should be 15789 was " + peakTable.getValue("Intensity", 0));
+		assertTrue( Math.abs( 3500.0 - peakTable.getValue("baseline", 0) ) < 1 , "Peak baseline is off by more than the 1. Should be 3500 was " + peakTable.getValue("baseline", 0));
+		assertTrue( Math.abs( 3000.0 - peakTable.getValue("height", 0) ) < 1 , "Peak height is off by more than the 1. Should be 3000 was " + peakTable.getValue("height", 0));
+		assertTrue( Math.abs( 1.2 - peakTable.getValue("sigma", 0) ) < TOLERANCE , "Peak sigma is off by more than the 10. Should be 3000 was " + peakTable.getValue("sigma", 0));
+		assertTrue( Math.abs( 1 - peakTable.getValue("R2", 0) ) < TOLERANCE , "Peak R2 is off by more than the tolerance. Should be 1 was " + peakTable.getValue("sigma", 0));
+		
+		assertEquals(peakTable.getValue("T", 1), 0);
+		assertTrue( Math.abs( 43.7 - peakTable.getValue("x", 1) ) < TOLERANCE , "Peak x position is off by more than the tolerance. Should be 43.7 was " + peakTable.getValue("x", 1));
+		assertTrue( Math.abs( 26.7 - peakTable.getValue("y", 1) ) < TOLERANCE , "Peak y position is off by more than the tolerance. Should be 26.7 was " + peakTable.getValue("y", 1));
+		assertTrue( Math.abs( 14756.0 - peakTable.getValue("Intensity", 1) ) < TOLERANCE , "Peak Intensity is off by more than the tolerance. Should be 14756 was " + peakTable.getValue("Intensity", 1));
+		assertTrue( Math.abs( 3500.0 - peakTable.getValue("baseline", 1) ) < 1 , "Peak baseline is off by more than the 1. Should be 3500 was " + peakTable.getValue("baseline", 1));
+		assertTrue( Math.abs( 3000.0 - peakTable.getValue("height", 1) ) < 1 , "Peak height is off by more than the 1. Should be 3000 was " + peakTable.getValue("height", 1));
+		assertTrue( Math.abs( 1.2 - peakTable.getValue("sigma", 1) ) < TOLERANCE , "Peak sigma is off by more than the 10. Should be 3000 was " + peakTable.getValue("sigma", 1));
+		assertTrue( Math.abs( 1 - peakTable.getValue("R2", 1) ) < TOLERANCE , "Peak R2 is off by more than the tolerance. Should be 1 was " + peakTable.getValue("sigma", 1));
+		
+		assertEquals(peakTable.getValue("T", 2), 0);
+		assertTrue( Math.abs( 32.5 - peakTable.getValue("x", 2) ) < TOLERANCE , "Peak x position is off by more than the tolerance. Should be 43.7 was " + peakTable.getValue("x", 2));
+		assertTrue( Math.abs( 40 - peakTable.getValue("y", 2) ) < TOLERANCE , "Peak y position is off by more than the tolerance. Should be 26.7 was " + peakTable.getValue("y", 2));
+		assertTrue( Math.abs( 14416.0 - peakTable.getValue("Intensity", 2) ) < TOLERANCE , "Peak Intensity is off by more than the tolerance. Should be 14756 was " + peakTable.getValue("Intensity", 2));
+		assertTrue( Math.abs( 3500.0 - peakTable.getValue("baseline", 2) ) < 1 , "Peak baseline is off by more than the 1. Should be 3500 was " + peakTable.getValue("baseline", 2));
+		assertTrue( Math.abs( 3000.0 - peakTable.getValue("height", 2) ) < 1 , "Peak height is off by more than the 1. Should be 3000 was " + peakTable.getValue("height", 2));
+		assertTrue( Math.abs( 1.2 - peakTable.getValue("sigma", 2) ) < TOLERANCE , "Peak sigma is off by more than the 10. Should be 3000 was " + peakTable.getValue("sigma", 2));
+		assertTrue( Math.abs( 1 - peakTable.getValue("R2", 2) ) < TOLERANCE , "Peak R2 is off by more than the tolerance. Should be 1 was " + peakTable.getValue("sigma", 2));
+
+		assertEquals(peakTable.getValue("T", 3), 1);
+		assertTrue( Math.abs( 10 - peakTable.getValue("x", 3) ) < TOLERANCE , "Peak x position is off by more than the tolerance. Should be 10 was " + peakTable.getValue("x", 3));
+		assertTrue( Math.abs( 10 - peakTable.getValue("y", 3) ) < TOLERANCE , "Peak y position is off by more than the tolerance. Should be 10 was " + peakTable.getValue("y", 3));
+		assertTrue( Math.abs( 15789.0 - peakTable.getValue("Intensity", 3) ) < TOLERANCE , "Peak Intensity is off by more than the tolerance. Should be 15789 was " + peakTable.getValue("Intensity", 3));
+		assertTrue( Math.abs( 3500.0 - peakTable.getValue("baseline", 3) ) < 1 , "Peak baseline is off by more than the 1. Should be 3500 was " + peakTable.getValue("baseline", 3));
+		assertTrue( Math.abs( 3000.0 - peakTable.getValue("height", 3) ) < 1 , "Peak height is off by more than the 1. Should be 3000 was " + peakTable.getValue("height", 3));
+		assertTrue( Math.abs( 1.2 - peakTable.getValue("sigma", 3) ) < TOLERANCE , "Peak sigma is off by more than the 10. Should be 3000 was " + peakTable.getValue("sigma", 3));
+		assertTrue( Math.abs( 1 - peakTable.getValue("R2", 3) ) < TOLERANCE , "Peak R2 is off by more than the tolerance. Should be 1 was " + peakTable.getValue("sigma", 3));
+		
+		assertEquals(peakTable.getValue("T", 4), 1);
+		assertTrue( Math.abs( 43.7 - peakTable.getValue("x", 4) ) < TOLERANCE , "Peak x position is off by more than the tolerance. Should be 43.7 was " + peakTable.getValue("x", 4));
+		assertTrue( Math.abs( 26.7 - peakTable.getValue("y", 4) ) < TOLERANCE , "Peak y position is off by more than the tolerance. Should be 26.7 was " + peakTable.getValue("y", 4));
+		assertTrue( Math.abs( 14756.0 - peakTable.getValue("Intensity", 4) ) < TOLERANCE , "Peak Intensity is off by more than the tolerance. Should be 14756 was " + peakTable.getValue("Intensity", 4));
+		assertTrue( Math.abs( 3500.0 - peakTable.getValue("baseline", 4) ) < 1 , "Peak baseline is off by more than the 1. Should be 3500 was " + peakTable.getValue("baseline", 4));
+		assertTrue( Math.abs( 3000.0 - peakTable.getValue("height", 4) ) < 1 , "Peak height is off by more than the 1. Should be 3000 was " + peakTable.getValue("height", 4));
+		assertTrue( Math.abs( 1.2 - peakTable.getValue("sigma", 4) ) < TOLERANCE , "Peak sigma is off by more than the 10. Should be 3000 was " + peakTable.getValue("sigma", 4));
+		assertTrue( Math.abs( 1 - peakTable.getValue("R2", 4) ) < TOLERANCE , "Peak R2 is off by more than the tolerance. Should be 1 was " + peakTable.getValue("sigma", 4));
+		
+		assertEquals(peakTable.getValue("T", 5), 1);
+		assertTrue( Math.abs( 32.5 - peakTable.getValue("x", 5) ) < TOLERANCE , "Peak x position is off by more than the tolerance. Should be 43.7 was " + peakTable.getValue("x", 5));
+		assertTrue( Math.abs( 40 - peakTable.getValue("y", 5) ) < TOLERANCE , "Peak y position is off by more than the tolerance. Should be 26.7 was " + peakTable.getValue("y", 5));
+		assertTrue( Math.abs( 14416.0 - peakTable.getValue("Intensity", 5) ) < TOLERANCE , "Peak Intensity is off by more than the tolerance. Should be 14756 was " + peakTable.getValue("Intensity", 5));
+		assertTrue( Math.abs( 3500.0 - peakTable.getValue("baseline", 5) ) < 1 , "Peak baseline is off by more than the 1. Should be 3500 was " + peakTable.getValue("baseline", 5));
+		assertTrue( Math.abs( 3000.0 - peakTable.getValue("height", 5) ) < 1 , "Peak height is off by more than the 1. Should be 3000 was " + peakTable.getValue("height", 5));
+		assertTrue( Math.abs( 1.2 - peakTable.getValue("sigma", 5) ) < TOLERANCE , "Peak sigma is off by more than the 10. Should be 3000 was " + peakTable.getValue("sigma", 5));
+		assertTrue( Math.abs( 1 - peakTable.getValue("R2", 5) ) < TOLERANCE , "Peak R2 is off by more than the tolerance. Should be 1 was " + peakTable.getValue("sigma", 5));
 
 	}
 
 	public Dataset simulateDataset() {
-		// Using the same seed each time ensure we always get
-		// the same sequence of random numbers
-		Random r = new Random(25);
-
-		long[] dim = { 50, 50, 50 };
+		long[] dim = { 50, 50, 2 };
 		AxisType[] axes = { Axes.X, Axes.Y, Axes.TIME };
-		Dataset dataset = datasetService.create(dim, "simulated Image", axes, 16,
+		Dataset dataset = datasetService.create(dim, "Simulated Image", axes, 16,
 			false, false);
-
+		
 		for (int t = 0; t < dim[2]; t++)
-			for (int x = 0; x < dim[0]; x++)
-				for (int y = 0; y < dim[1]; y++) {
-					Gaussian2D peak1 = new Gaussian2D(1000d, 3000d, 10d, 10d + t / 4,
-						1.2d);
-					Gaussian2D peak2 = new Gaussian2D(1000d, 3000d, 32.5d, 40d + t / 4,
-						1.2d);
-					Gaussian2D peak3 = new Gaussian2D(1000d, 3000d, 43.7d, 26.7d + t / 4,
-						1.2d);
-					Gaussian2D peak4 = new Gaussian2D(1000d, 1000d, 17.2d, 30d, 1.2d);
-					Gaussian2D peak5 = new Gaussian2D(1000d, 1000d, 12.3d, 13.5d, 1.2d);
-					Gaussian2D peak6 = new Gaussian2D(1000d, 2000d, 35.3d, 13.5d, 4d);
-
-					dataset.getImgPlus().randomAccess().setPositionAndGet(x, y, t)
-						.setReal(r.nextInt(1000) + 500 + peak1.getValue(x, y) + peak2
-							.getValue(x, y) + peak3.getValue(x, y) + peak4.getValue(x, y) +
-							peak5.getValue(x, y) + peak6.getValue(x, y));
-				}
+			for (int x=0; x < dim[0]; x++)
+				 for (int y=0; y < dim[1]; y++) {
+					Gaussian2D peak1 = new Gaussian2D(1000d, 3000d, 10d, 10d, 1.2d);
+				 	Gaussian2D peak2 = new Gaussian2D(1000d, 3000d, 32.5d, 40d, 1.2d);
+				 	Gaussian2D peak3 = new Gaussian2D(1000d, 3000d, 43.7d, 26.7d, 1.2d);
+				 	
+				 	dataset.getImgPlus().randomAccess().setPositionAndGet(x, y, t).setReal(500 
+						+ peak1.getValue(x, y)
+						+ peak2.getValue(x, y)
+						+ peak3.getValue(x, y));	
+				 }
+		
 		return dataset;
 	}
 }
