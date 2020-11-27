36b3dd1df307db5e439259504bad786e98ca56b2
Karl Duderstadt <duderstadt@biochem.mpg.de>
2020-11-27 14:33:42 +0100

    clear all OME metadata before repopulating in populateMetadata function

--- src/main/java/de/mpg/biochem/mars/metadata/AbstractMarsMetadata.java
+++ src/main/java/de/mpg/biochem/mars/metadata/AbstractMarsMetadata.java
@@ -137,6 +137,8 @@ public abstract class AbstractMarsMetadata extends AbstractMarsRecord implements
 	}
 
 	public void populateMetadata(OMEXMLMetadata md) {
+		images.clear();
+		
 		for (int imageIndex = 0; imageIndex < md.getImageCount(); imageIndex++)
 			images.put(imageIndex, new MarsOMEImage(imageIndex, md));
 	}
