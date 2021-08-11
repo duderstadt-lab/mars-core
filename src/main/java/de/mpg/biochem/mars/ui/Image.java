2e73f1cb3837c408f4aee5729c0dc01dde8099d4
Karl Duderstadt <duderstadt@biochem.mpg.de>
2021-08-11 07:55:43 +0200

    In progress commit, add SwingImageWidget and related classes

---
+++ src/main/java/de/mpg/biochem/mars/ui/Image.java
@@ -0,0 +1,13 @@
+package de.mpg.biochem.mars.ui;
+
+import org.scijava.Optional;
+
+/**
+ * A interface used for marking input fields of plugin parameter dialogs as
+ * image fields.
+ * 
+ * @author Karl Duderstadt
+ */
+public interface Image extends Optional {
+	// marker : nothing to implement
+}
---
+++ src/main/java/de/mpg/biochem/mars/ui/ImageWidget.java
@@ -0,0 +1,12 @@
+package de.mpg.biochem.mars.ui;
+
+import org.scijava.widget.InputWidget;
+
+/**
+ * Widget interface for message labels.
+ * 
+ * @author Curtis Rueden
+ */
+public interface ImageWidget<U> extends InputWidget<Image, U> {
+	// NB: No changes to interface.
+}
